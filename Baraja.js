79b31bfb408cd508601ec851d4f8cda7b9eab841
A01375000 <30907377+Gigio212@users.noreply.github.com>
2021-10-20 21:59:51 -0500

    Add files via upload

--- Baraja.js
+++ Baraja.js
@@ -1,17 +1,6 @@
-const mongoose = require('mongoose')
-const Baraja = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    Color:{
-        type: String,
-        required: true
-    },
-    Palo:{
-        type: String,
-        required: false
-    },Numeracion:{
-        type: String,
-        required: false
-    }
-},{collection:'Baraja'})
+const router = require("express").Router();
+const BarajaController = require('../controllers/Baraja');
 
-module.exports = mongoose.model('Baraja',Baraja)
+router.post('/agregarBaraja',BarajaController.postAgregarBaraja)
+router.get('/obtenerBaraja',BarajaController.getObtenerBaraja)
+module.exports = router
